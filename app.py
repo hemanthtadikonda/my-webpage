017a09406c7a83dbdb8027fe93521167da031171
hemanth <hemanthtadikonda29@gmail.com>
2024-07-27 14:55:23 +0530

    my webpage

--- app.py
+++ app.py
@@ -90,7 +90,7 @@ with st.container():
     st.write("---")
     left_column, right_column = st.columns(2)
     with left_column:
-        st.header("My Contributions :man_technologist:")
+        st.header("My Contributions :collision:")
         st.write("##")
         st.write(
         '''
