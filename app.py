4dfc1687d8107321cba7acf45d03cf8e2a50bc6a
hemanth <hemanthtadikonda29@gmail.com>
2024-07-27 12:02:00 +0530

    my webpage

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 import streamlit as st
 
-
 st.set_page_config(page_title="My Webpage",page_icon=":tada:",layout="wide")
 
 ## --- Header Section ---
-
 with st.container():
     st.subheader("Hi, I am Hemanth :wave: ")
     st.title("A DevOps Engineer from HCL Tech")
