b41bc50c444b3719939ced344737f85959134266
hemanth <hemanthtadikonda29@gmail.com>
2024-07-27 12:33:20 +0530

    my webpage

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
+import requests
 import streamlit as st
+from streamlit_lottie import st_lottie
+
 
 st.set_page_config(page_title="My Webpage",page_icon=":tada:",layout="wide")
 
+def load_lottieurl(url):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.mp4()
+
+# --- Load Assets---
+
+lottie_coding = "https://cdnl.iconscout.com/lottie/premium/thumb/professional-male-devops-8872610-7233289.mp4"
+
 ## --- Header Section ---
+
 with st.container():
     st.subheader("Hi, I am Hemanth :wave: ")
     st.title("A DevOps Engineer from HCL Tech")
@@ -13,8 +27,8 @@ with st.container():
 
 with st.container():
     st.write("---")
-    left_column, right_column = st.columns(2)
-    with left_column:
+    left_column,right_column = st.columns(2)
+    with left_coulmn:
         st.header("What i do")
         st.write("##")
         st.write(
@@ -27,3 +41,5 @@ with st.container():
             '''
         )
         st.write("[Know More >](https://github.com/hemanthtadikonda)")
+    with right_column:
+        st_lottie(lottie_coding, height=300, key="coding")
