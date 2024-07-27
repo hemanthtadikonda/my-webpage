9cc396fd6a4937ccb6e9f80f6deb3e98870c4ec7
hemanth <hemanthtadikonda29@gmail.com>
2024-07-27 11:55:47 +0530

    my webpage

--- app.py
+++ app.py
@@ -10,3 +10,23 @@ with st.container():
     st.title("A DevOps Engineer from HCL Tech")
     st.write(" I am passinate about automate deployments ")
     st.write("[Laern More >](https://github.com/hemanthtadikonda)")
+
+## ---- what i do ----
+
+with st.container():
+    st.write("---")
+    left_colum,right_colum = st.colum (2)
+
+    with left_coulm:
+        st.header("What i do")
+        st.write("##")
+        st.write(
+            '''
+            Continuous Integration and Continuous Deployment (CI/CD):
+            Infrastructure as Code (IaC):
+            Configuration Management:
+            Monitoring and Logging:
+            Containerization and Orchestration:
+            '''
+        )
+        st.write("[Know More >](https://github.com/hemanthtadikonda)")
