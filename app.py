75cc85716db98790e88a2ca7c54100be1215b440
hemanth <hemanthtadikonda29@gmail.com>
2024-07-27 11:49:17 +0530

    my webpage

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ import streamlit as st
 st.set_page_config(page_title="My Webpage",page_icon=":tada:",layout="wide")
 
 ## --- Header Section ---
-st.subheader("Hi, I am Hemanth :wave: ")
-st.title("A DevOps Engineer from HCL Tech")
-st.write(" I am passionate about automate deployments ")
-st.write("[Learn More >](https://github.com/hemanthtadikonda)")
+
+with st.container():
+    st.subheader("Hi, I am Hemanth :wave: ")
+    st.title("A DevOps Engineer from HCL Tech")
+    st.write(" I am passinate about automate deployments ")
+    st.write("[Laern More >](https://github.com/hemanthtadikonda)")
