d19044ec37136c3ef7b871c1785eb97602e91e36
hemanth <hemanthtadikonda29@gmail.com>
2024-07-27 14:26:30 +0530

    my webpage

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ img_contact_from = Image.open("images/Hemanth.png")
 ## --- Header Section ---
 with st.container():
     st.subheader("Hi, I am Hemanth Tadikonda :wave: ")
-    st.title("A DevOps Engineer from HCL Tech :laptop: ")
-    st.subheader(" I am a passionate DevOps Engineer with a keen interest in automating deployments and optimizing the software delivery pipeline. :laptop:")
+    st.title("A DevOps Engineer from HCL Tech 🇮🇳 ")
+    st.write(" I am a passionate DevOps Engineer with a keen interest in automating deployments and optimizing the software delivery pipeline.")
     st.write("[About HCL Tech >](https://en.wikipedia.org/wiki/HCLTech)")
 
 ## ---- About Me ---
