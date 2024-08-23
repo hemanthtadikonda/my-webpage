44fdb947652924eededa3067dbefc10e17172c17
hemanth <hemanthtadikonda29@gmail.com>
2024-08-23 19:12:52 +0530

    my webpage ci

---
+++ terraform/vpc/main.tf
@@ -0,0 +1,13 @@
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.1/24"
+}
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.0.1/29"
+  availability_zone = us-east-1
+
+  tags = {
+    Name = "Main"
+  }
+}
