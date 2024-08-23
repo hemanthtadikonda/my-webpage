40cfecbdb07654dff3b124380e3cb76a28d29bc9
hemanth <hemanthtadikonda29@gmail.com>
2024-08-23 19:52:05 +0530

    my webpage ci

--- terraform/vpc/main.tf
+++ terraform/vpc/main.tf
@@ -1,7 +1,7 @@
 
 
 resource "aws_subnet" "main" {
-  for_each = each.value
+  for_each = var.vpc_id
   vpc_id     = var.vpc_id
   cidr_block = each.value["cidr"]
   availability_zone = each.value["az"]
