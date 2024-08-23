b44fa0c236d03bed273699b9e8296ac760bbc5db
hemanth <hemanthtadikonda29@gmail.com>
2024-08-23 19:26:06 +0530

    my webpage ci

--- terraform/vpc/main.tf
+++ terraform/vpc/main.tf
@@ -4,7 +4,7 @@ resource "aws_vpc" "main" {
 
 resource "aws_subnet" "main" {
   vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.0.1/29"
+  cidr_block = "10.0.0.0/26"
   availability_zone = us-east-1
 
   tags = {
