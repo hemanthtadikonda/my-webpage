cdece31480e7289aaeb4cd220165a1b204ebc681
hemanth <hemanthtadikonda29@gmail.com>
2024-08-24 09:20:47 +0530

    my webpage cloudformation

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,6 +9,9 @@ module "vpc" {
 
   tags = var.tags
   env  = var.env
+  default_vpc_id = var.default_vpc_id
+  default_vpc_cidr_block = var.default_vpc_cidr_block
+  def_route_table_id    = var.def_route_table_id
 
 
 
--- terraform/vars.tf
+++ terraform/vars.tf
@@ -1,3 +1,6 @@
 variable "vpc" {}
 variable "tags" {}
 variable "env" {}
+variable "default_vpc_id" {}
+variable "default_vpc_cidr_block" {}
+variable "def_route_table_id" {}
