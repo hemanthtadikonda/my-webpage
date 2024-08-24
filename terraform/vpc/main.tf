8f9bd2968355d24049bc48a42d5485fd1c083522
hemanth <hemanthtadikonda29@gmail.com>
2024-08-24 08:44:28 +0530

    my webpage cloudformation

--- terraform/vpc/main.tf
+++ terraform/vpc/main.tf
@@ -11,10 +11,15 @@ module "subnets" {
 
 }
 
-resource "aws_internet_gateway" "gw" {
+resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
+  tags   = { Name = "myapp-igw" }
+}
 
-  tags = {
-    Name = "main"
-  }
+resource "aws_route" "igw" {
+  for_each               = lookup(lookup(module.subnets,"public",null ),"route_table_ids",null)
+
+  route_table_id         = each.value["id"]
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
 }
