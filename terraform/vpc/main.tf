152d26c63330a95d76182f814ef1b7ffb6e6b657
hemanth <hemanthtadikonda29@gmail.com>
2024-08-24 08:31:59 +0530

    my webpage cloudformation

--- terraform/vpc/main.tf
+++ terraform/vpc/main.tf
@@ -10,3 +10,11 @@ module "subnets" {
   subnets  = each.value
 
 }
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main"
+  }
+}
--- terraform/vpc/subnets/main.tf
+++ terraform/vpc/subnets/main.tf
@@ -8,3 +8,15 @@ resource "aws_subnet" "main" {
     Name = each.key
   }
 }
+
+resource "aws_route_table" "main" {
+  for_each = var.subnets
+  vpc_id   = var.vpc_id
+  tags     = { Name = "${each.key}-rt" }
+}
+
+resource "aws_route_table_association" "association" {
+  for_each = var.subnets
+  subnet_id      = aws_subnet.main.id
+  route_table_id = aws_route_table.main.id
+}
