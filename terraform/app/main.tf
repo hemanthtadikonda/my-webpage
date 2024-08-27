660813ff0180c3d89dd009a5b3c10cb66e1b8d9a
hemanth <hemanthtadikonda29@gmail.com>
2024-08-27 12:19:47 +0530

    my webpage

--- terraform/app/main.tf
+++ terraform/app/main.tf
@@ -76,7 +76,7 @@ resource "aws_lb_target_group" "main" {
     healthy_threshold   = 2
     interval            = 10
     path                = "/"
-    port                = var.app_listen_port
+    port                = 80
     timeout             = 2
     unhealthy_threshold = 2
     matcher             = "200"
